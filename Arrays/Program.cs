ab1d377375f81351bed45883dcbe732fb3ca9b20
Dəmi̇r Paşayev <dpashayev.30960@naa.edu.az>
2024-07-11 12:45:28 +0400

    First commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double de,de1,de2,x1,x2;
+			double[,] matris = new double[2,3];
+			for (int i = 0; i < 2; i++)
+            {
+				for (int j = 0; j < 3; j++)
+				{
+					matris[i, j] = double.Parse(Console.OpenStandardInput().ToString());
+				}
+			}
+				de = (matris[0,0] * matris[1,1]) - (matris[0,1] * matris[1,0]);
+				de1 = (matris[0,2] * matris[1,1]) - (matris[0,1] * matris[1,2]);
+				de2 = (matris[0,0] * matris[1,2]) - (matris[0,2] * matris[1,0]);
+				x1 = de1 / de;
+				x2 = de2 / de;
+			NumberFormatInfo setPrecision = new NumberFormatInfo();
+			setPrecision.NumberDecimalDigits = 3;
+			Console.WriteLine(x1.ToString("N",setPrecision));
+			Console.WriteLine(x2.ToString("N", setPrecision));
+			Console.ReadKey();
+		}
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //{Ref,Out əməliyyatı}
+            //int a = 20, b = 10;
+            //Console.WriteLine(Tpl(ref a, b));
+            //Console.WriteLine(a);
+            Console.WriteLine(vrm(2, 4));
+            Console.WriteLine(Add(1,2,50));
+            Console.ReadLine();
+        }
+        static int vrm(int say1, int say2, int say3=1)
+        {
+            return say1 * say2 * say3;
+        }
+        //static int Tpl(ref int say1,int say2)
+        //{
+        //    say1 = 5;
+        //    return say1 + say2;
+        //}
+        static int Add(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
